e8f2e854ada0a06f7a45a34cd81631bc2a9042eb
Gwamaka Charles <gwaxart19@gmail.com>
2019-11-12 13:02:06 +0300

    Updates

---
+++ precache-manifest.3d1ec07bfd5c700dd79e316b29c9c34b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "f177a88a1132f56e9415",
+    "url": "/Edit_Text/static/css/main.1fce9140.chunk.css"
+  },
+  {
+    "revision": "f177a88a1132f56e9415",
+    "url": "/Edit_Text/static/js/main.f177a88a.chunk.js"
+  },
+  {
+    "revision": "19a844d6453d16ac7dc6",
+    "url": "/Edit_Text/static/css/1.00e2d154.chunk.css"
+  },
+  {
+    "revision": "19a844d6453d16ac7dc6",
+    "url": "/Edit_Text/static/js/1.19a844d6.chunk.js"
+  },
+  {
+    "revision": "9e85e6b4a29276ae8679",
+    "url": "/Edit_Text/static/js/runtime~main.9e85e6b4.js"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/Edit_Text/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/Edit_Text/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/Edit_Text/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/Edit_Text/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/Edit_Text/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "2ba81615eb45b666739a0dea2b438b0c",
+    "url": "/Edit_Text/index.html"
+  }
+];
